d0f296f6d1a286acafbeecf16ad58ea41252e112
Sharp <sharpanims@gmail.com>
2021-12-02 20:15:17 +0800

    Program

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Code__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double num01;
+            Double num02;
+            Double num03;
+            
+
+            System.Console.WriteLine("Good Morning, What numbers would you like to find the average of");
+
+            System.Console.WriteLine("Number 1: ");
+            num01 = Convert.ToDouble(System.Console.ReadLine() );
+
+            System.Console.WriteLine("Number 2: ");
+            num02 = Convert.ToDouble(System.Console.ReadLine() );
+
+            System.Console.WriteLine("Number 3: ");
+            num03 = Convert.ToDouble(System.Console.ReadLine() );
+
+            Double average = (num01 + num02 + num03) / 3;
+  
+
+            System.Console.WriteLine("The Average is: " + average);
+
+        
+        
+        }
+    }
+}
